11c38d666461425392f2625e05e7d783cd0d2440
Kimosable <129333154+Kimosable@users.noreply.github.com>
2023-05-30 15:39:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 60: Сформируйте трёхмерный массив из неповторяющихся 
+// двузначных чисел. Напишите программу, которая будет построчно выводить 
+// массив, добавляя индексы каждого элемента.
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int[,,] arr = new int[10, 10, 10];
+        Random rnd = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    int num = rnd.Next(10, 100);
+
+                    while (Array.IndexOf(arr, num) != -1)
+                    {
+                        num = rnd.Next(10, 100);
+                    }
+
+                    arr[i, j, k] = num;
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int k = 0; k < 10; k++)
+                {
+                    Console.WriteLine("arr[{0}, {1}, {2}] = {3}", i, j, k, arr[i, j, k]);
+                }
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
